629cf492ad053b45e718dd2ad43d2ec33a4ab25f
Ivan Litvinov <IvanLitvinov1357@yandex.ru>
2023-11-06 23:51:36 +0300

    rewrote data analyze, retrained the models with better dataset and fixed docker-compose

--- posts_feed_service/lib/utils.py
+++ posts_feed_service/lib/utils.py
@@ -44,7 +44,7 @@ def request_transform(
     request_df["weekday"] = request["request_time"].weekday()
 
     if exp_group == "test":
-        text_w2v_cols = [f"desc_w2v_{i}" for i in range(1, 11)]
+        text_w2v_cols = [f"text_w2v_{i}" for i in range(1, 11)]
         cols = ['post_id'] + text_w2v_cols
         request_df = request_df.merge(
             tables["embs_post_df"][cols], on="post_id"
